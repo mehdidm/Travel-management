b581f2e062d8136776c5df85cc59f42e7a24e838
Mehdi <dimassimehdi58@gmail.com>
2023-04-07 11:09:08 +0000

    WIP:Spring service discovery

---
+++ Hotel-MS/src/main/java/com/ms/hotelms/Response/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.ms.hotelms.Response;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- Hotel-MS/src/main/java/com/ms/hotelms/service/impl/ReservationServiceImpl.java
+++ Hotel-MS/src/main/java/com/ms/hotelms/service/impl/ReservationServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ms.hotelms.service.impl;
 
+import com.ms.hotelms.Response.FraudCheckResponse;
 import com.ms.hotelms.entity.ReservationEntity;
 import com.ms.hotelms.repository.ReservationRepository;
 import com.ms.hotelms.service.ReservationService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,9 +13,11 @@ import java.util.Optional;
 @Service
 public class ReservationServiceImpl implements ReservationService {
     private final ReservationRepository reservationRepository;
+    private final RestTemplate restTemplate ;
 
-    public ReservationServiceImpl(ReservationRepository reservationRepository) {
+    public ReservationServiceImpl(ReservationRepository reservationRepository, RestTemplate restTemplate) {
         this.reservationRepository = reservationRepository;
+        this.restTemplate = restTemplate;
     }
 
 
@@ -29,7 +33,17 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public ReservationEntity saveReservation(ReservationEntity reservationEntity) {
-        return reservationRepository.save(reservationEntity);
+        ReservationEntity resrvation = reservationRepository.saveAndFlush(reservationEntity);
+        FraudCheckResponse fraudChckResponse = restTemplate.getForObject(
+                "http://FRAUD-MS/api/v1/fraud-check{clientId}",
+                FraudCheckResponse.class,
+                resrvation.getId()
+
+        );
+        if (fraudChckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        return reservationEntity  ;
     }
 
     @Override
