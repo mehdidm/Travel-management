2d2964716bdec01161463d03201f78ec1899312c
mahdibenamor15 <mahdi.benamor@esprit.tn>
2023-04-26 19:47:53 +0100

    initializing the trip ms project

---
+++ Trip-MS/src/main/java/com/example/tripsms/TripsMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsMsApplication.class, args);
+	}
+
+}
