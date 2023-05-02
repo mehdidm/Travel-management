2dc0512e875654c735da16ff3a694f1f94c8cd05
Mahdi Ben Amor <mahdi.benamor@medianet.com.tn>
2023-05-02 23:41:06 +0100

    add the ms to the docker compose file

--- Trip-MS/src/main/java/com/example/tripms/TripMsApplication.java
+++ Trip-MS/src/main/java/com/example/tripms/TripMsApplication.java
@@ -2,8 +2,10 @@ package com.example.tripms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TripMsApplication {
 
     public static void main(String[] args) {
