ec7be1d5abc82fea1f5aab1b2733426859c1740a
arwa.lemaalem <arwa.lemaalem@medianet.com.tn>
2023-05-02 17:30:10 +0100

    wip

--- Agency-MS/src/main/java/com/example/agent/AgentApplication.java
+++ Agency-MS/src/main/java/com/example/agent/AgentApplication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AgentApplication {
 	public static void main(String[] args) {
