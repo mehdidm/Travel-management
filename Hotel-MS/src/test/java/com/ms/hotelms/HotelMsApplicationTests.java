2bc23037a7ea836c0991643b7b9ecaa1da89b4a3
arwa.lemaalem <arwa.lemaalem@medianet.com.tn>
2023-05-03 08:00:01 +0100

    wip

--- Hotel-MS/src/test/java/com/ms/hotelms/HotelMsApplicationTests.java
+++ Hotel-MS/src/test/java/com/ms/hotelms/HotelMsApplicationTests.java
@@ -1,13 +1,1 @@
-package com.ms.hotelms;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HotelMsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
